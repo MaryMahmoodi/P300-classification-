2927c9c2783b625c05790e94ac38f8131ea641dc
MaryMahmoodi <72376694+MaryMahmoodi@users.noreply.github.com>
2021-11-06 09:01:09 +0330

    Update Main_P300_featureextraction_classification.m

--- Main_P300_featureextraction_classification.m
+++ Main_P300_featureextraction_classification.m
@@ -14,7 +14,7 @@
 % P300 evokes in your EEG signal around 300 ms after you watch 
 % a picture, primarily defined in your mind (target picture).
 % If the target picture consists of characters, it can be used in a BCI speller system designed for writing with your mind.
-% How? The code detects P300 and the corresponding set of characters is typed on the screen.
+% How? The code detects the presence of P300 on the EEG in the duration of each stimulus image, and the corresponding set of characters is typed on the screen at the end of a trial.
 
 % First, the P300 pattern is highlighted in the EEG signal. 
 %To do this, raw signals are denoised using transient suppression-based convex optimization and thresholding-based high amplitude artifact suppression.
@@ -96,7 +96,7 @@ signal  = fix_damagedelectrodes( signal,Rereference );
 tic,
 %%% initialize BP filter parameters %%%
 
-% the Parks-McClellan method is used via the ‘remez’ function of MATLAB
+% the Parks-McClellan method is used via the â€˜remezâ€™ function of MATLAB
 rp = 0.01; % Passband ripple
 rs = 26; % Stopband ripple
 f = freqrange; % Cutoff frequencies
